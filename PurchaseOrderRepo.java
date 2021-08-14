03e534653db317c5429ecc6e18ea9be2b7b9e76e
Noufmalajmi <70598033+Noufmalajmi@users.noreply.github.com>
2021-08-15 01:06:40 +0300

    Add files via upload

---
+++ PurchaseOrderRepo.java
@@ -0,0 +1,15 @@
+package repo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sportyshoes.model.PurchaseOrder;
+
+
+@Repository
+public interface PurchaseOrderRepo extends CrudRepository<PurchaseOrder, Integer> {
+
+
+}
+
+
