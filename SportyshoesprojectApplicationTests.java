57dc5ffa158930de3a71ef784c8f532c3498e358
Noufmalajmi <70598033+Noufmalajmi@users.noreply.github.com>
2021-08-15 01:09:07 +0300

    Add files via upload

---
+++ SportyshoesprojectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.SportyShoesProject;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyshoesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
